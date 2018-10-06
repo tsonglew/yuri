86a6dc87f73a02d82bc81b1d4d8718508caa5cbe
kasheemlew <417879751@qq.com>
2018-10-06 16:36:50 +0800

    add scout

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import cv2
 import numpy as np
 
 import sc2
-from sc2 import run_game, Race, maps, Difficulty
+from sc2 import run_game, Race, maps, Difficulty, position
 from sc2.player import Bot, Computer
 from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, \
     CYBERNETICSCORE, STALKER, STARGATE, VOIDRAY, OBSERVER, ROBOTICSFACILITY
@@ -24,6 +24,7 @@ class MainBot(sc2.BotAI):
 
     async def on_step(self, iteration):
         self.iteration = iteration
+        await self.intel()
         await self.distribute_workers()
         await self.build_workers()
         await self.build_pylons()
@@ -32,7 +33,7 @@ class MainBot(sc2.BotAI):
         await self.offensive_force_building()
         await self.build_offensive_force()
         await self.attack()
-        await self.intel()
+        await self.scout()
 
     async def intel(self):
         game_data = np.zeros(
@@ -40,26 +41,75 @@ class MainBot(sc2.BotAI):
             dtype=np.uint8
         )
 
+        # draw own
         draw_dict = {
             NEXUS: [15, (0, 255, 0)],
             PYLON: [3, (20, 235, 0)],
             PROBE: [1, (55, 200, 0)],
             ASSIMILATOR: [2, (55, 200, 0)],
+            OBSERVER: [1, (255, 255, 255)],
             GATEWAY: [3, (200, 100, 0)],
             CYBERNETICSCORE: [3, (150, 150, 0)],
+            ROBOTICSFACILITY: [5, (215, 155, 0)],
             STARGATE: [5, (255, 0, 0)],
             VOIDRAY: [3, (255, 100, 0)]
         }
         for unit_type in draw_dict:
             for unit in self.units(unit_type):
-                position = unit.position
+                pos = unit.position
                 cv2.circle(
                     game_data,
-                    (int(position[0]), int(position[1])),
+                    (int(pos[0]), int(pos[1])),
                     draw_dict[unit_type][0],
                     draw_dict[unit_type][1],
                     -1
                 )
+
+        # draw enemy buildings
+        main_base_names = ['nexus', 'commandcenter', 'hatchery']
+        for enemy_building in self.known_enemy_structures:
+            pos = enemy_building.position
+            if enemy_building.name.lower() in main_base_names:
+                cv2.circle(
+                    game_data,
+                    (int(pos[0]), int(pos[1])),
+                    15,
+                    (0, 0, 255),
+                    -1
+                )
+            else:
+                cv2.circle(
+                    game_data,
+                    (int(pos[0]), int(pos[1])),
+                    5,
+                    (200, 50, 212),
+                    -1
+                )
+        
+        # draw enemy units
+        for enemy_unit in self.known_enemy_units:
+            if not enemy_unit.is_structure:
+                worker_names = ['probe', 'scv', 'drone']
+                pos = enemy_unit.position
+                # draw workers
+                if enemy_unit.name.lower() in worker_names:
+                    cv2.circle(
+                        game_data, 
+                        (int(pos[0]), int(pos[1])), 
+                        1, 
+                        (55, 0, 155), 
+                        -1
+                    )
+                # draw else
+                else:
+                    cv2.circle(
+                        game_data, 
+                        (int(pos[0]), int(pos[1])), 
+                        3, 
+                        (50, 0, 215), 
+                        -1
+                    )
+
 
         flipped = cv2.flip(game_data, 0)
         resized = cv2.resize(flipped, dsize=None, fx=2, fy=2)
@@ -96,7 +146,6 @@ class MainBot(sc2.BotAI):
             for rf in self.units(ROBOTICSFACILITY).ready.noqueue:
                 if self.can_afford(OBSERVER) and self.supply_left > 0:
                     await self.do(rf.train(OBSERVER))
-
 
     async def build_workers(self):
         probe_nums = len(self.units(PROBE))
@@ -135,7 +184,7 @@ class MainBot(sc2.BotAI):
         if self.units(GATEWAY).ready.exists and not self.units(CYBERNETICSCORE):
             if self.can_afford(CYBERNETICSCORE) and not self.already_pending(CYBERNETICSCORE):
                 await self.build(CYBERNETICSCORE, near=pylon)
-        elif len(self.units(GATEWAY)) < 2:
+        elif self.units(GATEWAY).amount == 0:
             if self.can_afford(GATEWAY) and not self.already_pending(GATEWAY):
                 await self.build(GATEWAY, near=pylon)
         
@@ -143,6 +192,9 @@ class MainBot(sc2.BotAI):
             if len(self.units(STARGATE)) < self.iteration / self.IPS / 2:
                 if self.can_afford(STARGATE) and not self.already_pending(STARGATE):
                     await self.build(STARGATE, near=pylon)
+            if self.units(ROBOTICSFACILITY).amount == 0:
+                if self.can_afford(ROBOTICSFACILITY) and not self.already_pending(ROBOTICSFACILITY):
+                    await self.build(ROBOTICSFACILITY, near=pylon)
 
     async def build_offensive_force(self):
         # train stalkers
@@ -163,13 +215,17 @@ class MainBot(sc2.BotAI):
             return random.choice(self.known_enemy_structures)
         return self.enemy_start_locations[0]
 
-    async def offend(self, unit):
-        for s in self.units(unit).idle:
-            await self.do(s.attack(random.choice(self.known_enemy_units)))
+    async def offend(self, units):
+        for u in units:
+            for s in self.units(unit).idle:
+                await self.do(s.attack(random.choice(self.known_enemy_units)))
     
-    async def defend(self, unit):
-        for s in self.units(unit).idle:
-            await self.do(s.attack(self.find_target()))
+    async def defend(self, units):
+        if not isinstance(units, list):
+            units = [units]
+        for u in units:
+            for s in self.units(u).idle:
+                await self.do(s.attack(self.find_target()))
 
     async def attack(self):
         aggresive_units = {
@@ -187,4 +243,4 @@ if __name__ == '__main__':
     run_game(maps.get('AbyssalReefLE'), [
         Bot(Race.Protoss, MainBot()),
         Computer(Race.Terran, Difficulty.Hard)
-    ], realtime=True)
+    ], realtime=False)
